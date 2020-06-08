443c69cf52e29c8458879900ba2a5611ef032162
Ryno Myburgh <rynomyburgh@gmail.com>
2020-06-08 18:00:29 +0200

    Added better menu

--- src/App.vue
+++ src/App.vue
@@ -13,30 +13,36 @@
 
 <template>
   <div id="app">
-    <header class="masthead mb-auto">
-      <div class="inner">
-        <h3 class="masthead-brand">
-          Console
-        </h3>
-        <nav
-          id="nav"
-          class="nav nav-masthead justify-content-center"
-        >
-          <router-link
-            to="/"
-            class="nav-link"
-          >
-            Home
-          </router-link> | 
-          <router-link
-            to="/about"
-            class="nav-link"
-          >
-            About
-          </router-link>
-        </nav>
+    <nav class="navbar navbar-expand-sm navbar-dark">
+      <a class="navbar-brand" href="#">Game Console</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Games
+            </a>
+            <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+              <router-link
+                to="/"
+              >
+                <a class="dropdown-item dropdown-item-dark" href="#">Tetris <span class="sr-only">(current)</span></a>
+              </router-link>
+            </div>
+          </li>
+          <li class="nav-item">
+            <router-link
+              to="/about"
+            >
+              <a class="nav-link" href="#">About</a>
+            </router-link>
+          </li>
+        </ul>
       </div>
-    </header>
+    </nav>
 
     <main
       role="main"
